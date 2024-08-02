2193dee71386778b5532fa375417360832d89097
sbottingota <saku.bottingota@gmail.com>
2024-08-03 00:45:00 +0100

    Made q20 using gmp

---
+++ q20.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <gmp.h>
+
+#define LIMIT 100
+
+int main() {
+    mpz_t product;
+    mpz_init(product);
+    mpz_set_ui(product, 1);
+
+    for (int i = 2; i <= LIMIT; ++i) {
+        mpz_mul_si(product, product, i);
+    }
+
+    char *str = mpz_get_str(NULL, 10, product);
+    mpz_clear(product);
+
+    int sum = 0;
+    
+    for (char *c = str; *c != '\0'; ++c) {
+        sum += *c - '0';
+    }
+
+    printf("%d\n", sum);
+}
+    
