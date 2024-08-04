fc583c2697751ff7afbc4d00a738f2626f38c235
sbottingota <saku.bottingota@gmail.com>
2024-08-04 23:44:03 +0100

    Made q16

--- Makefile
+++ Makefile
@@ -1,3 +1,6 @@
 CC=gcc
 CFLAGS=-Wall -Wextra -O
 LDLIBS=-lm
+
+%: %.c %.txt
+	$(CC) $(CFLAGS) $@.c -o $@ $(LDLIBS)
---
+++ q16.c
@@ -0,0 +1,31 @@
+// N.B. this requires gmp to be installed and linked
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <gmp.h>
+
+#define LIMIT 1000
+
+int main() {
+    mpz_t product;
+    mpz_init(product);
+    mpz_set_si(product, 1);
+
+    for (int i = 0; i < LIMIT; ++i) {
+        mpz_mul_si(product, product, 2);
+    }
+
+    char *str = mpz_get_str(NULL, 10, product);
+    mpz_clear(product);
+
+    int sum = 0;
+
+    for (char *c = str; *c != '\0'; ++c) {
+        sum += *c - '0';
+    }
+
+    free(str);
+
+    printf("%d\n", sum);
+}
