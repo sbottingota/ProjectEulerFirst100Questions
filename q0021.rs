4d56f0881a0ddfe04a89ce3f05a9ca4289fa4476
Saku Botting-Ota <git@github.com>
2024-09-23 15:38:57 +0100

    Optimized runtime to around 3min

--- q0021.rs
+++ q0021.rs
@@ -1,28 +1,21 @@
-const LIMIT: u32 = 3000;
+use std::collections::BTreeMap;
+
+const LIMIT: u32 = 10000;
 
 fn main() {
     let mut amicables: Vec<u32> = Vec::new();
 
     for i in 2..LIMIT {
-        // println!("{:?}", get_factors(i));
-        if is_amicable(i) {
-            amicables.push(i);
-            println!("{}", i);
+        for j in i..LIMIT {
+            if are_amicable(i, j) {
+                amicables.push(i);
+                amicables.push(j);
+                println!("{} {}", i, j);
+            }
         }
     }
 
     println!("{}", amicables.into_iter().sum::<u32>());
-}
-
-// test if n is amicable with any number up to LIMIT
-fn is_amicable(n: u32) -> bool {
-    for m in 2..LIMIT {
-        if are_amicable(n, m) {
-            return true;
-        }
-    }
-
-    false
 }
 
 #[inline]
@@ -33,8 +26,14 @@ fn are_amicable(n: u32, m: u32) -> bool {
     n != m  && get_factors(n).into_iter().sum::<u32>() == m && get_factors(m).into_iter().sum::<u32>() == n
 }
 
+const factors_cache: BTreeMap<u32, Vec<u32>> = BTreeMap::new();
+
 // note that function does not include the number itself in it's return value
 fn get_factors(n: u32) -> Vec<u32> {
+    if factors_cache.contains_key(&n) {
+        return factors_cache.get(&n).unwrap().to_vec();
+    }
+
     let mut factors: Vec<u32> = vec![1];
     let upper_bound = (n as f64).sqrt() as u32 + 1;
     for i in 2..upper_bound {
@@ -45,6 +44,8 @@ fn get_factors(n: u32) -> Vec<u32> {
             }
         }
     }
+
+    factors_cache.insert(n.clone(), factors.clone());
 
     factors
 }
