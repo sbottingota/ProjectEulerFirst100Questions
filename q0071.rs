8757285e128dfefa2b38650008a60966edd8fe60
sbottingota <saku.bottingota@gmail.com>
2024-11-10 17:54:53 +0000

    Made q71 not take 18 minutes

--- q0071.rs
+++ q0071.rs
@@ -4,25 +4,21 @@ const DENOM_LIMIT: f64 = 1e6;
 const IMMEDIATE_RIGHT: f64 = 3.0 / 7.0; // the fraction to the immediate right of the fraction we are trying to find.
 
 fn main() {
-    let mut best_num: f64 = 0.0;
-    let mut best_denom: f64 = 1.0;
+    let mut best_num: f64 = 1.0;
+    let mut best_frac: f64 = 0.0; // best_num/best_denom, to minimize calculatons
 
     let mut denom: f64 = 1.0;
     while denom <= DENOM_LIMIT {
-        let mut num: f64 = 1.0;
-
-        while num / denom < IMMEDIATE_RIGHT {
-            if num / denom > best_num / best_denom {
-                best_num = num;
-                best_denom = denom;
-            }
-
-            num += 1.0;
+        let num: f64 = (denom * IMMEDIATE_RIGHT).floor(); // very vague approximation
+        if num / denom > best_frac && num / denom < IMMEDIATE_RIGHT {
+            best_num = num;
+            best_frac = num / denom;
         }
+
 
         denom += 1.0;
     }
 
-    println!("{}/{}", best_num, best_denom);
+    println!("{}", best_num);
 }
 
